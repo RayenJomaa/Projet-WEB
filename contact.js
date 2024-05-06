bba7e86c297d19290e002b54408e11f7e6eaa0d4
NassimYounes <nassim.55.younes@gmail.com>
2024-05-07 00:27:59 +0100

    finally

--- contact.js
+++ contact.js
@@ -1,45 +1,9 @@
-
-var facebookIcon = document.getElementById("facebook");
-var instagramIcon = document.getElementById("instagram");
-var twitterIcon = document.getElementById("twitter");
-
-facebookIcon.addEventListener("mouseover", agrandirIcone);
-instagramIcon.addEventListener("mouseover", agrandirIcone);
-twitterIcon.addEventListener("mouseover", agrandirIcone);
-
-function agrandirIcone(event) {
-    event.target.style.transform = "scale(3)";
-    event.target.style.color ="#00BFFF"
-}
-
-facebookIcon.addEventListener("mouseout", retrecirIcone);
-instagramIcon.addEventListener("mouseout", retrecirIcone);
-twitterIcon.addEventListener("mouseout", retrecirIcone);
-
-function retrecirIcone(event) {
-    event.target.style.transform = "scale(1)";
-    event.target.style.color ="white" ;
-}
-
-var submit = document.getElementById("submit");
-
-
-submit.addEventListener("mouseover", function() {
-    this.style.transform = "scale(1.1)";
-});
-submit.addEventListener("mouseout", function() {
-    this.style.transform = "scale(1)";
-});
-
-
-
 function tester() {
     var firstName = document.getElementById("first_name").value;
     var lastName = document.getElementById("last_name").value;
     var email = document.getElementById("email").value;
     var phoneNumber = document.getElementById("phone_number").value;
     var message = document.getElementById("Message").value;
-  
 
     var errorMessage = "";
 
@@ -75,6 +39,36 @@ function tester() {
         return false; 
     }
 
-    
     return true;
 }
+
+var facebookIcon = document.getElementById("facebook");
+var instagramIcon = document.getElementById("instagram");
+var twitterIcon = document.getElementById("twitter");
+
+facebookIcon.addEventListener("mouseover", agrandirIcone);
+instagramIcon.addEventListener("mouseover", agrandirIcone);
+twitterIcon.addEventListener("mouseover", agrandirIcone);
+
+function agrandirIcone(event) {
+    event.target.style.transform = "scale(3)";
+    event.target.style.color = "#00BFFF";
+}
+
+facebookIcon.addEventListener("mouseout", retrecirIcone);
+instagramIcon.addEventListener("mouseout", retrecirIcone);
+twitterIcon.addEventListener("mouseout", retrecirIcone);
+
+function retrecirIcone(event) {
+    event.target.style.transform = "scale(1)";
+    event.target.style.color = "white";
+}
+
+var submit = document.getElementById("submit");
+
+submit.addEventListener("mouseover", function() {
+    this.style.transform = "scale(1.1)";
+});
+submit.addEventListener("mouseout", function() {
+    this.style.transform = "scale(1)";
+});
